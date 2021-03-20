bf91abb3eed6464cae9938d21c9426e6a3feed8d
Alailson <aalailson3@gmail.com>
2021-03-20 17:11:17 +0000

    make templates

--- commands/manage_commands.py
+++ commands/manage_commands.py
@@ -1,2 +1,9 @@
-def commands_init(msg):
-    return 'workifdsfdsafsng'
+def start_battle_controller(update):
+    return 'with the update - {}'.format(update)
+
+def commands_init(update, command):
+    switcher = {
+        "/start-new-battle": start_battle_controller(update),
+        "/stop-battle": "u selected stop new battle",
+    }
+    return switcher.get(command, "Incorrect selection")
