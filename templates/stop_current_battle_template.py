257c451bc911826cf51d3c5ab8a26ad6bfa13d7e
Alailson <aalailson3@gmail.com>
2021-03-21 23:35:23 +0000

    implements stop current battle template

---
+++ templates/stop_current_battle_template.py
@@ -0,0 +1,11 @@
+def stop_current_battle_template(update):
+    return """
+            <b>--------LOG_INFO <u>current battle stopped</u>---------</b>
+            <b>    </b>
+            <strong>ENGLISH BATTLE STOPPED</strong>
+            <b>-------------------------------------------</b>
+            <strong>start a new one</strong>
+            <strong>just type /start-new-battle</strong>
+<b>-----------------------------------------------------------------</b>
+<i>just type /help-english-battle to know how to play</i>
+"""
