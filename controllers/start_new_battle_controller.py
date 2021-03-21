0055bd687e031018220e054c74df721877dce4d8
Alailson <aalailson3@gmail.com>
2021-03-21 19:39:45 +0000

    implements start new battle controller

--- controllers/start_new_battle_controller.py
+++ controllers/start_new_battle_controller.py
@@ -1,5 +1,30 @@
+from setup_app.app_init import mongo
+from templates.start_new_battle_template import start_new_battle_template
+
 def start_new_battle_controller(update):
-    print(update)
-    return """
-       <code>with the update - {}'</code> 
-    """.format(update)
+    existUser = mongo.db.users.find_one({
+        'username': "{}".format(update.message.from_user.username),
+        'chat_id': "{}".format(update.message.chat.id)
+        })
+    existBattle = mongo.db.battles.find_one({
+        'group_id': "{}".format(update.message.chat.id),
+    })
+    if existUser is not None and existBattle is not None:
+        if existBattle['battle_status'] == 'waiting':
+            return start_new_battle_template(update)
+
+        if existBattle['battle_status'] == 'started':
+            return 'battle already started - you are currently playing with someone'
+        result = mongo.db.battles.update_one(
+            #find
+            {'_id': existBattle['_id']}, 
+            #update
+            {"$set": { 
+                'battle_status': 'waiting',
+                'player_one': "{}".format(update.message.from_user.username)
+                }
+            })
+
+        return start_new_battle_template(update)
+    else:
+        return 'not found'
